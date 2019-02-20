df9860d8b910d8731636082782b6b1aa0363a768
WinHong\liwh <liwh@winhong.com>
2019-02-20 18:01:38 +0800

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { CommonsModule } from './common/common.module';
+import { BackRoutingModule } from './back/backRouter.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
+    CommonsModule,
+    BackRoutingModule,
+    FrontModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, IndexComponent]
 })
 export class AppModule { }
---
+++ src/app/common/common.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+    declarations: [
+        NavComponent,
+        LoginComponent
+    ],
+    imports: [ CommonModule ],
+    exports: [],
+    providers: [],
+})
+export class CommonsModule {}
